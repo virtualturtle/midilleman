5abe271e21f8ad14431a988f89b4b0cd43124157
virtualturtle <noahmesh@gmail.com>
2018-01-10 22:40:25 -0500

    Added NLA track separation

--- script.py
+++ script.py
@@ -1,40 +1,60 @@
 import bpy
 import csv
+from bpy import context
 
 frame_tick = 7.5729
-delay = 30
-object = 'Cube'
-animation = 'CubeAction'
-channel = 4
+delay = 29
+object = 'Bass 3'
+animation = 'Bass3'
+channel = "4"
 note = " 60"
 frame = 0
 animlength = 69
 lastframe = (int(animlength) * -1 - 1)
 bpy.data.objects[object].select = True
+bpy.context.area.type = 'NLA_EDITOR'
+
+def nlaselect():
+    ob = context.object
+    ad = ob.animation_data
+    if ad:
+        for i, track in enumerate(ad.nla_tracks):
+            # select all whose name starts with "Nla"
+            track.select = track.name.startswith("Nla")
+            # make active track if in pos 0
+            if track.select and not i:
+                ad.nla_tracks.active = track
+                
+nlaselect()
 
 def dupe(row):
-    #bpy.ops.anim.channels_select_all_toggle()
-    print("Note Overlap by " + str((frame - lastframe) * 1 ) + " frames. Duplicating.")
     bpy.ops.object.duplicate()
+
     print(bpy.context.selected_objects)
+
     bpy.context.area.type = 'NLA_EDITOR'
+
     for obj in bpy.context.selected_objects:
-        obj.animation_data_clear()
-        bpy.ops.nla.selected_objects_add()
-        obj.animation_data.nla_tracks.new()
-    print("animation cleared.")
-    bpy.ops.nla.tracks_delete()
-    for obj in bpy.context.selected_objects:
-        obj.animation_data.nla_tracks.new()
-    bpy.context.scene.frame_set(int(row[1])/frame_tick)
-    bpy.ops.nla.actionclip_add(action=animation)
+        obj.animation_data_clear()          #clear animation data
+        bpy.ops.nla.selected_objects_add()  #add blank animation data
+        obj.animation_data.nla_tracks.new() #add new NLA track
+    print("animation cleaned.")
+
     bpy.ops.anim.channels_select_all_toggle()
-    print("added animation to dupe.")
-        
-def nodupe(row):  
-    print("no note overlap, continuing as normal.")
-    bpy.context.scene.frame_set(int(row[1])/frame_tick)
+
+    nlaselect()
+                
+    bpy.context.scene.frame_set((int(row[1])/frame_tick - delay + 39))
     bpy.ops.nla.actionclip_add(action=animation)
+    print("added animation to dupe.")
+    bpy.context.area.type = 'TEXT_EDITOR'
+        
+def nodupe(row):
+    bpy.context.area.type = 'NLA_EDITOR'  
+    print("no note overlap, continuing as normal.")
+    bpy.context.scene.frame_set((int(row[1])/frame_tick - delay + 39))
+    bpy.ops.nla.actionclip_add(action=animation)
+    bpy.context.area.type = 'TEXT_EDITOR'
         
 def keyframe(row):
     global frame
@@ -56,7 +76,7 @@ def main():
         read = csv.reader(ifile, delimiter = ',', quotechar = "'")
         print("opened file.")
         for row in read :
-            if row[0] == "4" and row[2] == " Note_on_c" and row[4] == note and row[1] != 0:
+            if row[0] == channel and row[2] == " Note_on_c" and row[4] == note and row[1] != 0:
                 print("conditions met.")
                 frame = (int(row[1]) / frame_tick)
                 print("frame: " + str(frame))
@@ -68,6 +88,11 @@ def main():
 			
 		
 if __name__ == "__main__":
-    bpy.context.area.type = 'NLA_EDITOR'
+
     main()
+    bpy.context.area.type = 'NLA_EDITOR'
+    bpy.ops.anim.channels_select_all_toggle()
+    bpy.ops.nla.select_all_toggle(invert=False)
+    bpy.context.area.type = 'VIEW_3D'
+    bpy.ops.object.select_all(action='TOGGLE')
     bpy.context.area.type = 'TEXT_EDITOR'
